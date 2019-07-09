52b419aed3775a6297a7e1e9e2670fcd7396aefe
szachn-u <ugo.szachnowski@hotmail.fr>
2019-07-09 22:56:40 +0200

    Create coverage.py

---
+++ coverage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 22:07:07 2019
+
+@author: ugo
+"""
+
+##########
+#  libs  #
+##########
+
+import sys
+import numpy as np
+import os
+import json
+import pyBigWig
+
+###########
+# classes #
+###########
