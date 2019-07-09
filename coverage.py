66b36e45842219e743e3f5910b83d8e098003e31
szachn-u <ugo.szachnowski@hotmail.fr>
2019-07-09 23:02:03 +0200

    Update coverage.py

--- coverage.py
+++ coverage.py
@@ -18,3 +18,30 @@ import pyBigWig
 ###########
 # classes #
 ###########
+
+class Layout:
+
+    def readGff(self, file, info = ["Name","Parent","gene","Alias","orf_classification","Ontology_term","Note","GO"]):
+        tmp = np.genfromtxt(f, dtype = 'str', delimiter = "\t")
+        coord = tmp[:,0:8]
+        notes = tmp[:, 8]
+
+        notes_ = ['']*len(notes)
+        annot = ['']*(len(notes)+1)
+        annot[0] = ["Chr", "Source", "Type", "Start", "Stop", "Score", "Strand", "Frame", "ID"] + info
+
+        for i_line in range(0,len(notes)):
+            info_ = ['']*len(info)
+            line_ = notes[i_line].split(";")
+            for col in line_:
+                split = col.split("=")
+                info_name = split[0]
+                info_val = split[1]        
+                try:
+                    ind_info = info.index(info_name)
+                    info_[ind_info] = info_val      
+                except:
+                    pass
+            notes_[i_line] = info_
+            annot[i_line+1] = coord[i_line].tolist() + notes_[i_line]     
+        self.annot = annot
